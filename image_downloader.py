3d2d1c7617bac99d0e7d932d1b51e42e395f61b3
Cory D. Wiles <kwylez@gmail.com>
2012-01-29 21:58:48 -0500

    basic fetching of latests posts from freshome

--- image_downloader.py
+++ image_downloader.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
+import httplib2
+from BeautifulSoup import BeautifulSoup, SoupStrainer
 from lib.utils.decorators import async
+
+
+FRESH_HOMES_ROOT_URL = 'http://freshome.com/'
 
 
 # Fetch Paths to Images
@@ -8,8 +13,17 @@ def fetch_image_paths():
     return 'Hello world'
 
 
-# Fetch Images for Paths
+def find_latest_posts():
+    http = httplib2.Http()
+    status, response = http.request(FRESH_HOMES_ROOT_URL)
+    soup = BeautifulSoup(response, parseOnlyThese=SoupStrainer('h2'))
+    return soup.findAll({'a': True})
 
 
 if __name__ == "__main__":
-    fetch_image_paths()
+
+    for link in find_latest_posts():
+        try:
+            print link['href']
+        except KeyError:
+            pass
