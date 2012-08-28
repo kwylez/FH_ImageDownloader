d091788a30e473c2b01c0f95e8d998716fa78d2f
Cory D. Wiles <kwylez@gmail.com>
2012-08-28 11:52:04 -0400

    script will now download the images synchronously. Need to start adding in the async functionality and better error handling

--- image_downloader.py
+++ image_downloader.py
@@ -1,29 +1,63 @@
 #!/usr/bin/env python
-import httplib2
-from BeautifulSoup import BeautifulSoup, SoupStrainer
-from lib.utils.decorators import async
 
+import httplib2
+import urllib
+
+from BeautifulSoup import BeautifulSoup, SoupStrainer
+# from lib.utils.decorators import async
 
 FRESH_HOMES_ROOT_URL = 'http://freshome.com/'
+DEBUG = False
 
 
 # Fetch Paths to Images
-@async
-def fetch_image_paths():
-    return 'Hello world'
+def fetch_image_paths(url):
+
+    http = httplib2.Http()
+    status, response = http.request(url)
+    soup = BeautifulSoup(response)
+    all_image_links = []
+
+    try:
+        entry_div = soup.findAll('div', attrs={'class': 'entry_text'})
+        for div in entry_div:
+            for paragraph in div.findAll("p"):
+                all_image_links = paragraph.findAll({'img': True})
+
+    except TypeError:
+        print "It failed getting from %s" % url
+
+    return all_image_links
+
+
+def download_image_for_url(url):
+    if DEBUG:
+        print "image to download %s\n" % url
+    image_file_name = url.split('/')[-1]
+    urllib.urlretrieve(url, image_file_name)
 
 
 def find_latest_posts():
     http = httplib2.Http()
     status, response = http.request(FRESH_HOMES_ROOT_URL)
-    soup = BeautifulSoup(response, parseOnlyThese=SoupStrainer('h2'))
-    return soup.findAll({'a': True})
+    soup = BeautifulSoup(response, parseOnlyThese=SoupStrainer('h1'))
+
+    # Find all the 'a' tags skipping the first which is the homepage
+    all_links = soup.findAll({'a': True})[1:]
+
+    return all_links
 
 
 if __name__ == "__main__":
 
+    print "Fetch all images to download...\n"
+
     for link in find_latest_posts():
         try:
             print link['href']
+            print "Fetching image for %s\n" % link['href']
+            for image_link in fetch_image_paths(link['href']):
+                print "Source link: %s" % image_link['src']
+                download_image_for_url(image_link['src'])
         except KeyError:
             pass
